49e4969fcaa5e7682b028771650daa029d3a30db
anuragvishwak <anuragvishwakarma4132@gmail.com>
2026-03-12 01:34:46 +0530

    made some major changes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
-import { FaAward, FaBars, FaHome, FaPhone } from "react-icons/fa";
+import { FaArrowLeft, FaAward, FaBars, FaHome, FaPhone } from "react-icons/fa";
 import { GiBigDiamondRing } from "react-icons/gi";
 import Collections from "./Collections";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Navbar() {
+  const current_location = useLocation();
+  const navigation = useNavigate();
   const [menuOpen, setmenuOpen] = useState(false);
   const [openingCollectionSection, setopeningCollectionSection] =
     useState(false);
   return (
     <div>
       <div className="fixed top-3 left-0  right-0 z-50 flex justify-center px-4">
-        <div className="bg-white flex border border-gray-300 items-center mx-5 sm:mx-0 justify-between rounded-full px-3 sm:px-8 py-3 w-full sm:w-6/12 shadow-lg">
+        <div className="bg-white flex border border-gray-300 items-center mx-5 sm:mx-0 justify-between rounded-full px-3 py-3 w-full sm:w-6/12 shadow-lg">
           <p className="font-bold text-[#d2b986] border-2 border-[#d2b986] px-4 py-1.5 rounded-full font-luxury text-lg sm:text-xl whitespace-nowrap">
             Mira Jewellers
           </p>
@@ -28,12 +31,21 @@ function Navbar() {
             <button>Contact</button>
           </div>
 
-          <button
-            className="sm:hidden border border-[#d2b986] rounded-full p-2"
-            onClick={() => setmenuOpen(!menuOpen)}
-          >
-            <FaBars size={20} className="text-[#d2b986]" />
-          </button>
+          <div className="flex items-center space-x-2 sm:hidden">
+            {current_location.pathname === '/ProductDetails'?
+            <button onClick={() => navigation("/")} className="relative z-[60]">
+              <div className="flex items-center space-x-2 border border-[#8b857c] text-[#8b857c] rounded-full p-2">
+                <FaArrowLeft size={20} className="" />
+              </div>
+            </button>
+            : ''}
+            <button
+              className="sm:hidden border border-[#d2b986] rounded-full p-2"
+              onClick={() => setmenuOpen(!menuOpen)}
+            >
+              <FaBars size={20} className="text-[#d2b986]" />
+            </button>
+          </div>
         </div>
       </div>
 
@@ -63,7 +75,6 @@ function Navbar() {
       )}
       {openingCollectionSection && (
         <Collections
-        
           setOpeningCollectionSection={setopeningCollectionSection}
         />
       )}
